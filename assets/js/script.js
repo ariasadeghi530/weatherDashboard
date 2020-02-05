5ee5d595f1bf69a4c3b70ecc94f208db89cd49ba
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-04 22:01:17 -0800

    added search history collection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 let appid = '10207dcb3a245bc5201ad3e3a6420064';
 
+
 document.getElementById('searchBtn').addEventListener('click', event => {
 event.preventDefault();
+document.getElementById('cardItem').style.visibility = 'visible';
+
 let coordinates = [];
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('search').value}&appid=${appid}`)
     .then(r => r.json())
@@ -9,24 +12,41 @@ let coordinates = [];
       console.log(data)
      coordinates.push(data.coord.lon);
      coordinates.push(data.coord.lat);
+
      fahrenheit = kelvinToFahrenheit(Number(data.main.temp))
+
      document.getElementById('cityName').textContent = data.name;
      document.getElementById('temp').textContent = fahrenheit;
      document.getElementById('humid').textContent = data.main.humidity;
      document.getElementById('windSpd').textContent = data.wind.speed
+
+     let cityLink = document.createElement('a');
+     cityLink.innerHTML = `
+     <a href="#!" class="collection-item" id=${data.name}>${data.name}</a>
+     `
+     document.getElementById('collections').append(cityLink);
+
+     document.getElementById('search').value = '';
+
       fetch(`http://api.openweathermap.org/data/2.5/uvi?appid=${appid}&lat=${coordinates[1]}&lon=${coordinates[0]}`)
         .then(r => r.json())
         .then(data => {
-          console.log(data);
+
           document.getElementById('uv').textContent = data.value;
         })
         .catch(e => console.error(e));
      
-     console.log(coordinates)
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?appid=${appid}&lat=${coordinates[1]}&lon=${coordinates[0]}`)
+          .then(r => r.json())
+          .then(data => {
+      
+            console.log(data);
+          })
+          .catch(e => console.error(e));
     })
     .catch(e => console.error(e))
 
-    document.getElementById('search').value = '';
+
 })
 
 function kelvinToFahrenheit(num){
